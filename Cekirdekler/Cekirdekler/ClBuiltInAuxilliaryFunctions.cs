8b7d65b723fe21806945f6021df37ab2fbe9e627
tugrul512bit <huseyin.tugrul.buyukisik@gmail.com>
2017-04-15 15:01:41 +0300

    added base of clBuiltInHelper for future helper functions to be added before the kernel string (optionally)

---
+++ Cekirdekler/Cekirdekler/ClBuiltInAuxilliaryFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cekirdekler
+{
+    namespace ClBuiltInHelper
+    {
+        /// <summary>
+        /// <para>Only setting some flags will add some functions to beginning of OpenCL kernel string before compile</para>
+        /// <para>Not implemented(yet)</para>
+        /// </summary>
+        public class ClBuiltInAuxilliaryFunctions
+        {
+
+            private StringBuilder selectedFunctionsBuilder = new StringBuilder(@"");
+            private bool prepared = false;
+
+            private string exampleFunctionString = Environment.NewLine+ @"void exampleFunction(int a,int b){return a+b;}"+Environment.NewLine;
+            private bool exampleFunctionPrivate = false;
+            /// <summary>
+            /// adds "void exampleFunction(int a,int b){return a+b;}" in the beginning of kernel string
+            /// </summary>
+            public bool exampleFunction {
+                get { if (!prepared) { selectedFunctionsBuilder.Append(exampleFunctionString); } return exampleFunctionPrivate; }
+                set { exampleFunctionPrivate = value; }
+            }
+        }
+    }
+}
